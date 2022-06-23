c69edc30128561bfa4a46994d430f0b675404e73
Evil Dragon <48235622+EvilDragonFly@users.noreply.github.com>
2022-06-23 10:58:41 +0800

    Update 547.省份数量.cc

--- leetcode/547.省份数量.cc
+++ leetcode/547.省份数量.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-// 并查集   注意动态数组初始化，()和{}用于内置类型和自定义类型初始化的区别
+// 并查集||dfs   注意动态数组初始化，()和{}用于内置类型和自定义类型初始化的区别
 class Solution
 {
 public:
@@ -29,6 +29,61 @@ public:
             if (i != idx && isConnected[idx][i] == 1 && !visited[i])
                 dfs(isConnected, visited, i);
         }
+    }
+};
+
+class Solution1 {
+private:
+    int n;  // 并查集边的数量
+    vector<int> father;
+
+    // 并查集初始化
+    void init()
+    {
+        father = vector<int>(n);
+        for (int i = 0; i < n; ++i)
+            father[i] = i;
+    }
+    // 并查集里寻根的过程, 同时会更新寻根链路中各节点的根/路径压缩
+    int find(int u)
+    {
+        return u == father[u] ? u : father[u] = find(father[u]);
+    }
+    // 将v->u 这条边加入并查集
+    void join(int u, int v)
+    {
+        u = find(u);
+        v = find(v);
+        if (u == v)
+            return;
+        father[v] = u;
+    }
+    // 判断 u 和 v是否找到同一个根，本题用不上
+    bool same(int u, int v)
+    {
+        u = find(u);
+        v = find(v);
+        return u == v;
+    }
+
+public:
+    int findCircleNum(vector<vector<int>> &isConnected)
+    {
+        n = isConnected.size();
+        init();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j])
+                    join(i, j);
+            }
+        }
+        // 此时已经并查集已经把边链接好了
+
+        unordered_set<int> uset;  // 统计有多少个没有链接的省份
+        for (int i = 0; i < n; i++) {
+            uset.insert(find(i));
+        }
+        return uset.size();
     }
 };
 int main()
